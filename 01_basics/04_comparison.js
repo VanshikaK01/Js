241e0c5baf0224994e5d89084cf094f4287ed74f
VanshikaK01 <123823495+VanshikaK01@users.noreply.github.com>
2024-08-22 16:48:07 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,9 @@
+console.log(null>0) //false
+console.log(null==0) //false
+console.log(null>=0) //true because == and <> works differently comparisons convert null to a number treating it as 0
+
+console.log("2">1)
+console.log("02">1)
+
+//=== (strictly check karta h mtlb datatype bhi check karega)
+console.log("2"===2)
