1430d9af9473a0a1d36bfa82d0ed47cc04bf5afe
VanshikaK01 <123823495+VanshikaK01@users.noreply.github.com>
2024-08-22 16:17:17 +0000

    conversion

--- 01_basics/03_conversionOperator.js
+++ 01_basics/03_conversionOperator.js
@@ -31,3 +31,33 @@ let someNumber=33
 let stringNumber= String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+//*************operations*********** */
+
+let value=3
+let negValue= -value
+console.log(negValue)
+
+console.log(2**3) //2 to the power 3
+console.log(2/3);
+console.log(2%3)
+
+str1="hello"
+str2=" vanshika"
+let str3=str1+str2
+console.log(str3)
+
+console.log("1"+2+2) //pehla number string h toh sab string jaise treat honge 122
+console.log(1+2+"2") // pehla number so 32
+
+console.log(+true) //+ for conversion to number
+
+let x=3;
+const y=x++;
+console.log(`x:${x}, y:${y}`)
+
+let a=3;
+const b=++a;
+
+console.log(`a:${a}, b${b}`)
+
